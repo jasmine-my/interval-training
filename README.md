80396198f2be61e7bc47a0d36e0c615259783439
nae_yeonkyeong <yeonkyeong@vgen.co.kr>
2022-06-03 16:16:06 +0900

    리드미 수정

--- README.md
+++ README.md
@@ -2,28 +2,6 @@
 
 This README would normally document whatever steps are necessary to get your application up and running.
 
-### What is this repository for? ###
+### 프로젝트 목적 ###
 
-* Quick summary
-* Version
-* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
-
-### How do I get set up? ###
-
-* Summary of set up
-* Configuration
-* Dependencies
-* Database configuration
-* How to run tests
-* Deployment instructions
-
-### Contribution guidelines ###
-
-* Writing tests
-* Code review
-* Other guidelines
-
-### Who do I talk to? ###
-
-* Repo owner or admin
-* Other community or team contact
+* 내가 사용하려고 만든 인터벌 타이머 기능
--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,13 @@ import '~/global_styles/globalFonts.scss';
 import '~/global_styles/format_styles.css';
 import '~/global_styles/colors.scss';
 
-import NumberWithUnit from '~/components/NumberWithUnit/NumberWithUnit';
-import { UNIT } from '~/hooks/useConversion';
-
 const cx = className.bind(style);
 
 function App() {
     return (
         <div className="App">
-            <NumberWithUnit total={2} unit={UNIT.WON} actual={1} />
             <header className="App-header">
-                <img
-                    src={'/assets/images/icon-user.svg'}
-                    className="App-logo"
-                    alt="logo"
-                />
-                <p className={cx('widget')}>테스트(widget)</p>
+                <p className={cx('widget')}>Welcome to Interval Training</p>
                 <p className={cx('callout')}>테스트(Callout1)</p>
             </header>
         </div>
